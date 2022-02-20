a982bc58422e90f59ac47c0d831b42c1c4323ec8
sne12345 <sne12345@naver.com>
2022-02-21 01:17:53 +0900

    sds 알고리즘 특강 첫째주 못푼문제 풀기 : 조합, 백트래킹

---
+++ java-algorithm-study/src/DAY05/P15664/Main.java
@@ -0,0 +1,68 @@
+package DAY05.P15664;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    /*
+        - 백트래킹
+        - 조합
+     */
+
+    public static int N, M;
+    public static ArrayList<Integer> arr;
+    public static boolean [] possible;
+    public static ArrayList<String> answerList;
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("src/DAY05/P15664/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new ArrayList<>();
+        possible = new boolean[N];
+        answerList = new ArrayList<>();
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            arr.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Collections.sort(arr);
+        backTracking(0, M, new int[M], 0);
+        Set<String> answerSet = new LinkedHashSet<>(answerList);
+        answerList = new ArrayList<>(answerSet);
+
+        for(int i=0; i<answerList.size(); i++){
+            System.out.println(answerList.get(i));
+        }
+
+
+    }
+
+    public static void backTracking(int curIdx, int totalCnt, int [] sequence, int startIdx){
+        if(curIdx == totalCnt){
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<totalCnt; i++){
+                sb.append(sequence[i] + " ");
+            }
+            answerList.add(sb.toString());
+        } else {
+            for(int i=startIdx; i<N; i++){
+                if(!possible[i]) {
+                    sequence[curIdx] = arr.get(i);
+                    possible[i] = true;
+                    backTracking(curIdx + 1, totalCnt, sequence, i+1);
+                    possible[i] = false;
+                }
+            }
+        }
+    }
+}
